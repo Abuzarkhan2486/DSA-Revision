5b8960f5f32191408eafae0adbb61a43901a7c01
Your New Username <abuzarkhan2486@gmail.com>
2026-05-15 19:21:46 +0530

    first question

---
+++ Day-1.js
@@ -0,0 +1,28 @@
+
+//Fizz Buzz 
+
+let num = 25
+
+
+if (num % 3 === 0 || num % 5 === 0) {
+   
+     if (num % 3 === 0 && num % 5 === 0) {
+       return console.log("fizzandbuzz");
+        
+    }
+
+    if (num % 3 === 0) {
+       return console.log("fizz");
+
+    }
+    if (num % 5 === 0) {
+      return  console.log("buzz");
+
+    }
+}
+
+let arr=[10,15,20,20,55]
+
+
+
+
