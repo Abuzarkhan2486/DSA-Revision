63063f7d3f7d8ba7a83bc9e1ea6375641c137988
Your New Username <abuzarkhan2486@gmail.com>
2026-05-17 14:54:07 +0530

    solve array question

---
+++ Day-3.js
@@ -0,0 +1,31 @@
+// Reverse of an given array
+
+
+let arr = [29,39,65,80,37,45,23,56]
+
+let newarr=[]
+
+for(i=arr.length-1;i>=0;i--){
+    
+     newarr.push(arr[i])
+    
+}
+
+console.log(newarr);
+
+
+// Sum of all element 
+
+let arr2 = [29,39,65,80,37,45,23,56]
+
+let sum = 0
+
+for(i=arr.length-1;i>=0;i--){
+    
+     sum= sum + arr2[i]
+    
+}
+ console.log(sum);
+ 
+
+ // Find Smallest Number in a given array 
