c4e31fa49149c6dea308150e18d1419b5c1fc87f
Your New Username <abuzarkhan2486@gmail.com>
2026-05-18 19:53:57 +0530

    question

---
+++ Day-4.js
@@ -0,0 +1,15 @@
+let str = "abuzarkahan"
+let totalVovels=0
+for(i=0; i<str.length;i++){
+
+    if(str[i]==="a" ||str[i]==="e" ||str[i]==="i" ||str[i]==="o"|| str[i]==="u" ){
+        
+       totalVovels++
+        
+    }
+    
+
+}
+console.log(totalVovels);
+
+
